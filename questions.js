9a39981de82fbb9b7bc616d1b8762306cc948874
lkhyoung3-png <lkhyoung3@gmail.com>
2026-05-11 20:18:16 +0900

    Add files via upload

--- questions.js
+++ questions.js
@@ -17,15 +17,7 @@ const questions = [
       { text: "자주 있다", score: 2 },
       { text: "매일 한다", score: 3 }
     ]
-  },
-
-  {
-    question: "업무 집중이 잘 안 된다",
-    answers: [
-      { text: "아니다", score: 0 },
-      { text: "보통", score: 1 },
-      { text: "자주", score: 2 },
-      { text: "심각하다", score: 3 }
-    ]
   }
 ];
+
+window.questions = questions;
